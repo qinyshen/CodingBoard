c752923798efdff54cfcd653cfbd6b1498f00639
qinyshen <qinyshen@umich.edu>
2018-08-29 21:03:10 -0400

    let

---
+++ src/com/company/LeetCode/DuplicateSubtreesBiggerThanFour.java
@@ -0,0 +1,54 @@
+package com.company.LeetCode;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+
+public class DuplicateSubtreesBiggerThanFour {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+
+    boolean find = false;
+
+
+
+    public boolean  hasDuplicateSubtreesBiggerThanFour(TreeNode root) {
+        postorder(root, new HashSet<String>(), "");
+        return find;
+    }
+
+    public int postorder(TreeNode cur, HashSet<String> set,  String str) {
+        if(find) return 0;
+
+        if (cur == null){
+            return 0;
+        }
+
+        int length = 1;
+
+        String serial = cur.val + ",";
+
+        length += postorder(cur.left, set, serial);
+        length += postorder(cur.right, set, serial);
+
+        if(length >= 4){
+            if(set.contains(serial)){
+                find = true;
+                return 0;
+            }
+            else{
+                set.add(serial);
+            }
+        }
+
+        return length;
+    }
+
+
+
+}
